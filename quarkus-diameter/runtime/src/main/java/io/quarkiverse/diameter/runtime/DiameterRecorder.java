27e92bc6596d759bdffeebbf496efdc1c177214b
Eddie Carpenter <eddie.carpenter@frei.one>
2024-07-30 08:55:07 +1200

    Added default dictionary

--- quarkus-diameter/runtime/src/main/java/io/quarkiverse/diameter/runtime/DiameterRecorder.java
+++ quarkus-diameter/runtime/src/main/java/io/quarkiverse/diameter/runtime/DiameterRecorder.java
@@ -7,6 +7,7 @@ import io.quarkus.tls.TlsConfigurationRegistry;
 import io.smallrye.config.SmallRyeConfig;
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.jdiameter.api.*;
+import org.jdiameter.common.impl.validation.DictionaryImpl;
 import org.jdiameter.server.impl.StackImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,58 +19,60 @@ import java.util.function.Supplier;
 @Recorder
 public class DiameterRecorder
 {
-	private static final Logger LOG = LoggerFactory.getLogger(DiameterRecorder.class);
+    private static final Logger LOG = LoggerFactory.getLogger(DiameterRecorder.class);
 
-	public Function<SyntheticCreationalContext<Configuration>, Configuration> diameterConfiguration(Supplier<TlsConfigurationRegistry> registrySupplier, String configName)
-	{
-		return context -> {
-			LOG.info("Building Diameter configuration for profile '{}'", configName);
-			SmallRyeConfig config = ConfigProvider.getConfig().unwrap(SmallRyeConfig.class);
-			DiameterConfigs client = config.getConfigMapping(DiameterConfigs.class);
+    public Function<SyntheticCreationalContext<Configuration>, Configuration> diameterConfiguration(Supplier<TlsConfigurationRegistry> registrySupplier, String configName)
+    {
+        return context -> {
+            LOG.info("Building Diameter configuration for profile '{}'", configName);
+            SmallRyeConfig config = ConfigProvider.getConfig().unwrap(SmallRyeConfig.class);
+            DiameterConfigs client = config.getConfigMapping(DiameterConfigs.class);
 
-			DiameterDetailConfig diameterConfig = client.getDiameterConfig(configName);
-			if (diameterConfig == null) {
-				throw new IllegalArgumentException("No Diameter configuration found for profile '" + configName + "'");
-			}
+            DiameterDetailConfig diameterConfig = client.getDiameterConfig(configName);
+            if (diameterConfig == null) {
+                throw new IllegalArgumentException("No Diameter configuration found for profile '" + configName + "'");
+            }
 
-			return new DiameterConfiguration(diameterConfig, registrySupplier.get());
-		};
-	}
+            return new DiameterConfiguration(diameterConfig, registrySupplier.get());
+        };
+    }
 
-	public Function<SyntheticCreationalContext<Stack>, Stack> diameterStack(ShutdownContext shutdownContext, Supplier<TlsConfigurationRegistry> registrySupplier, String configName)
-	{
-		return context -> {
-			try {
-				LOG.info("Building Diameter Stack for configuration profile '{}'", configName);
-				SmallRyeConfig config = ConfigProvider.getConfig().unwrap(SmallRyeConfig.class);
-				DiameterConfigs client = config.getConfigMapping(DiameterConfigs.class);
+    public Function<SyntheticCreationalContext<Stack>, Stack> diameterStack(ShutdownContext shutdownContext, Supplier<TlsConfigurationRegistry> registrySupplier, String configName)
+    {
+        return context -> {
+            try {
+                DictionaryImpl.INSTANCE.setEnabled(true);
+				
+                LOG.info("Building Diameter Stack for configuration profile '{}'", configName);
+                SmallRyeConfig config = ConfigProvider.getConfig().unwrap(SmallRyeConfig.class);
+                DiameterConfigs client = config.getConfigMapping(DiameterConfigs.class);
 
-				DiameterDetailConfig diameterConfig = client.getDiameterConfig(configName);
-				if (diameterConfig == null) {
-					throw new IllegalArgumentException("No Diameter configuration found for profile '" + configName + "'");
-				}
+                DiameterDetailConfig diameterConfig = client.getDiameterConfig(configName);
+                if (diameterConfig == null) {
+                    throw new IllegalArgumentException("No Diameter configuration found for profile '" + configName + "'");
+                }
 
-				Stack stack = new StackImpl();
-				stack.init(new DiameterConfiguration(diameterConfig, registrySupplier.get()));
-				shutdownContext.addShutdownTask(() -> {
-					LOG.info("Stopping '{}' Diameter Stack", configName);
-					if (stack.isActive()) {
-						try {
-							stack.stop(10, TimeUnit.SECONDS, DisconnectCause.REBOOTING);
-						}
-						catch (IllegalDiameterStateException | InternalException ex) {
-							LOG.error("Error stopping Diameter Stack", ex);
-						}
+                Stack stack = new StackImpl();
+                stack.init(new DiameterConfiguration(diameterConfig, registrySupplier.get()));
+                shutdownContext.addShutdownTask(() -> {
+                    LOG.info("Stopping '{}' Diameter Stack", configName);
+                    if (stack.isActive()) {
+                        try {
+                            stack.stop(10, TimeUnit.SECONDS, DisconnectCause.REBOOTING);
+                        }
+                        catch (IllegalDiameterStateException | InternalException ex) {
+                            LOG.error("Error stopping Diameter Stack", ex);
+                        }
 
-						stack.destroy();
-					}
-				});
-				return stack;
-			}
-			catch (IllegalDiameterStateException | InternalException ex) {
-				LOG.error("Error creating '{}' Diameter Stack", configName, ex);
-				throw new DiameterSetupException("Error creating '" + configName + "' Diameter stack");
-			}
-		};
-	}
+                        stack.destroy();
+                    }
+                });
+                return stack;
+            }
+            catch (IllegalDiameterStateException | InternalException ex) {
+                LOG.error("Error creating '{}' Diameter Stack", configName, ex);
+                throw new DiameterSetupException("Error creating '" + configName + "' Diameter stack");
+            }
+        };
+    }
 }
