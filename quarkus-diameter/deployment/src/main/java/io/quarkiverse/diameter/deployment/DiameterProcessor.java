a30b01927af6ff29bb267790bd4d83e1ded26ac0
Eddie Carpenter <eddie.carpenter@frei.one>
2024-07-30 08:50:51 +1200

    Added more validation

--- quarkus-diameter/deployment/src/main/java/io/quarkiverse/diameter/deployment/DiameterProcessor.java
+++ quarkus-diameter/deployment/src/main/java/io/quarkiverse/diameter/deployment/DiameterProcessor.java
@@ -28,77 +28,127 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 import static io.quarkus.deployment.annotations.ExecutionTime.RUNTIME_INIT;
 
 class DiameterProcessor
 {
-	private static final Logger LOG = LoggerFactory.getLogger(DiameterProcessor.class);
-	private static final String FEATURE = "diameter";
-	private static final DotName DOTNAME_CONFIGURATION = DotName.createSimple("org.jdiameter.api.Configuration");
-	private static final DotName DOTNAME_STACK = DotName.createSimple("org.jdiameter.api.Stack");
-	private static final DotName DOTNAME_DIAMETER_SERVICE = DotName.createSimple("io.go.diameter.DiameterService");
-	private static final DotName DOTNAME_DIAMETER_SERVICE_OPTIONS = DotName.createSimple("io.go.diameter.DiameterServiceOptions");
-	private static final DotName DOTNAME_DIAMETER_SERVICE_INTERCEPTOR = DotName.createSimple("io.go.diameter.DiameterServiceInterceptor");
+    private static final Logger LOG = LoggerFactory.getLogger(DiameterProcessor.class);
+    private static final String FEATURE = "diameter";
+    private static final DotName DOTNAME_CONFIGURATION = DotName.createSimple("org.jdiameter.api.Configuration");
+    private static final DotName DOTNAME_STACK = DotName.createSimple("org.jdiameter.api.Stack");
+    private static final DotName DOTNAME_DIAMETER_SERVICE = DotName.createSimple("io.quarkiverse.diameter.DiameterService");
+    private static final DotName DOTNAME_DIAMETER_SERVICE_INTERCEPTOR = DotName.createSimple("io.quarkiverse.diameter.DiameterServiceInterceptor");
 
-	@BuildStep
-	FeatureBuildItem feature()
-	{
-		return new FeatureBuildItem(FEATURE);
-	}
+    private static final DotName DOTNAME_CLIENT_CAA_SESSION_LISTENER = DotName.createSimple("org.jdiameter.api.cca.ClientCCASessionListener");
+    private static final DotName DOTNAME_SERVER_CAA_SESSION_LISTENER = DotName.createSimple("org.jdiameter.api.cca.ServerCCASessionListener");
+    private static final DotName DOTNAME_CLIENT_GQ_SESSION_LISTENER = DotName.createSimple("org.jdiameter.api.gq.ClientGqSessionListener");
+    private static final DotName DOTNAME_SERVER_GQ_SESSION_LISTENER = DotName.createSimple("org.jdiameter.api.gq.ServerGqSessionListener");
+    private static final DotName DOTNAME_CLIENT_RX_SESSION_LISTENER = DotName.createSimple("org.jdiameter.api.rx.ClientRxSessionListener");
+    private static final DotName DOTNAME_SERVER_RX_SESSION_LISTENER = DotName.createSimple("org.jdiameter.api.rx.ServerRxSessionListener");
+    private static final DotName DOTNAME_CLIENT_S6A_SESSION_LISTENER = DotName.createSimple("org.jdiameter.api.s6a.ClientS6aSessionListener");
+    private static final DotName DOTNAME_SERVER_S6A_SESSION_LISTENER = DotName.createSimple("org.jdiameter.api.s6a.ServerS6aSessionListener");
+
+    private static final List<DotName> DIAMETER_SESSION_LISTENERS = List.of(DOTNAME_CLIENT_CAA_SESSION_LISTENER, DOTNAME_SERVER_CAA_SESSION_LISTENER, DOTNAME_CLIENT_GQ_SESSION_LISTENER, DOTNAME_SERVER_GQ_SESSION_LISTENER, DOTNAME_CLIENT_RX_SESSION_LISTENER, DOTNAME_SERVER_RX_SESSION_LISTENER, DOTNAME_CLIENT_S6A_SESSION_LISTENER, DOTNAME_SERVER_S6A_SESSION_LISTENER);
+
+    @BuildStep
+    public FeatureBuildItem feature()
+    {
+        return new FeatureBuildItem(FEATURE);
+    }
 
 
-	@Record(RUNTIME_INIT)
-	@BuildStep
-	ServiceStartBuildItem generateDiameterConfiguration(DiameterRecorder recorder, DiameterConfigs diameterConfig, ShutdownContextBuildItem shutdownContextBuildItem, TlsRegistryBuildItem tlsRegistryBuildItem, BuildProducer<SyntheticBeanBuildItem> syntheticBeanBuildItemBuildProducer)
-	{
-		if (diameterConfig.diameterConfigs() != null) {
-			for (String configName : diameterConfig.diameterConfigs().keySet()) {
-				if (Boolean.TRUE.equals(diameterConfig.diameterConfigs().get(configName).enabled())) {
-					syntheticBeanBuildItemBuildProducer.produce(createSyntheticBean(configName, Configuration.class, DOTNAME_CONFIGURATION, configName.equals(DiameterConfig.DEFAULT_CONFIG_NAME)).createWith(recorder.diameterConfiguration(tlsRegistryBuildItem.registry(), configName)).done());
+    @Record(RUNTIME_INIT)
+    @BuildStep
+    public ServiceStartBuildItem generateDiameterConfiguration(DiameterRecorder recorder,
+                                                               DiameterConfigs diameterConfig,
+                                                               ShutdownContextBuildItem shutdownContextBuildItem,
+                                                               TlsRegistryBuildItem tlsRegistryBuildItem,
+                                                               BuildProducer<SyntheticBeanBuildItem> syntheticBeanBuildItemBuildProducer)
+    {
+        if (diameterConfig.diameterConfigs() != null) {
+            for (String configName : diameterConfig.diameterConfigs().keySet()) {
+                if (Boolean.TRUE.equals(diameterConfig.diameterConfigs().get(configName).enabled())) {
+                    syntheticBeanBuildItemBuildProducer.produce(
+                            createSyntheticBean(configName,
+                                                Configuration.class,
+                                                DOTNAME_CONFIGURATION,
+                                                configName.equals(DiameterConfig.DEFAULT_CONFIG_NAME))
+                                    .createWith(recorder.diameterConfiguration(tlsRegistryBuildItem.registry(), configName))
+                                    .done());
 
-					syntheticBeanBuildItemBuildProducer.produce(createSyntheticBean(configName, Stack.class, DOTNAME_STACK, configName.equals(DiameterConfig.DEFAULT_CONFIG_NAME)).createWith(recorder.diameterStack(shutdownContextBuildItem, tlsRegistryBuildItem.registry(), configName)).done());
-				}//if
-			}//for
-		}//if
+                    syntheticBeanBuildItemBuildProducer.produce(
+                            createSyntheticBean(configName,
+                                                Stack.class,
+                                                DOTNAME_STACK,
+                                                configName.equals(DiameterConfig.DEFAULT_CONFIG_NAME))
+                                    .createWith(recorder.diameterStack(shutdownContextBuildItem, tlsRegistryBuildItem.registry(), configName))
+                                    .done());
+                }//if
+            }//for
+        }//if
 
-		return new ServiceStartBuildItem("DiameterService");
-	}
+        return new ServiceStartBuildItem("DiameterService");
+    }
 
-	@BuildStep
-	public void validateDiameterService(CombinedIndexBuildItem index, BuildProducer<ValidationPhaseBuildItem.ValidationErrorBuildItem> errors)
-	{
-		for (AnnotationInstance annotation : index.getIndex().getAnnotations(DOTNAME_DIAMETER_SERVICE)) {
-			if (!annotation.target().asClass().name().equals(DOTNAME_DIAMETER_SERVICE_INTERCEPTOR)) {
-				if (annotation.target().annotation(DOTNAME_DIAMETER_SERVICE_OPTIONS) == null) {
-					errors.produce(new ValidationPhaseBuildItem.ValidationErrorBuildItem(new DiameterSetupException("Missing @DiameterServiceOptions annotation on class " + annotation.target().asClass().name().withoutPackagePrefix())));
-				}
-			}
-		}
-	}
+    @BuildStep
+    public void validateDiameterService(CombinedIndexBuildItem index, BuildProducer<ValidationPhaseBuildItem.ValidationErrorBuildItem> errors)
+    {
+        for (AnnotationInstance annotation : index.getIndex().getAnnotations(DOTNAME_DIAMETER_SERVICE)) {
+            if (!annotation.target().asClass().name().equals(DOTNAME_DIAMETER_SERVICE_INTERCEPTOR)) {
+                List<DotName> interfaces = annotation.target().asClass().interfaceNames();
 
-	@BuildStep
-	void declareDiameterServicesAsBean(CombinedIndexBuildItem index, BuildProducer<AdditionalBeanBuildItem> additionalBeans)
-	{
-		List<String> diameterServices = index.getIndex().getKnownClasses().stream().filter(ci -> ci.hasAnnotation(DiameterService.class) && !ci.hasAnnotation(Interceptor.class)).map(ci -> ci.name().toString()).collect(Collectors.toList());
+                AtomicInteger count = new AtomicInteger(0);
+                interfaces.forEach(n -> {
+                    if (DIAMETER_SESSION_LISTENERS.contains(n)) {
+                        count.incrementAndGet();
+                    }
+                });
 
-		additionalBeans.produce(new AdditionalBeanBuildItem.Builder().addBeanClasses(diameterServices).setUnremovable().setDefaultScope(DotNames.APPLICATION_SCOPED).build());
-	}
+                if (count.get() > 1) {
+                    errors.produce(new ValidationPhaseBuildItem.ValidationErrorBuildItem(
+                            new DiameterSetupException("Only one XXXXSessionListener can be implemented per Diameter Service. " +
+                                                       "You will need to split the services into multiple services implementing only one XXXXSessionListener. " +
+                                                       "Remember that you cannot have more than one service implementing the same XXXXSessionListener.")));
+                }
+            }
+        }
+    }
 
-	private static <T> SyntheticBeanBuildItem.ExtendedBeanConfigurator createSyntheticBean(String clientName, Class<T> type, DotName exposedType, boolean isDefaultConfig)
-	{
-		LOG.info("Creating Synthetic Bean to {} for @DiameterClient(\"{}\")", type.getSimpleName(), clientName);
-		SyntheticBeanBuildItem.ExtendedBeanConfigurator configurator = SyntheticBeanBuildItem.configure(type).scope(ApplicationScoped.class).unremovable().setRuntimeInit().addType(exposedType);
+    @BuildStep
+    public void declareDiameterServicesAsBean(CombinedIndexBuildItem index, BuildProducer<AdditionalBeanBuildItem> additionalBeans)
+    {
+        List<String> diameterServices = index.getIndex()
+                                             .getKnownClasses()
+                                             .stream()
+                                             .filter(ci -> ci.hasAnnotation(DiameterService.class) && !ci.hasAnnotation(Interceptor.class))
+                                             .map(ci -> ci.name().toString())
+                                             .collect(Collectors.toList());
 
-		if (isDefaultConfig) {
-			configurator.defaultBean();
-			configurator.addQualifier(Default.class).addQualifier(DiameterConfig.class);
-		}
-		else {
-			configurator.addQualifier().annotation(DiameterConfig.class).addValue("value", clientName).done();
-		}
+        additionalBeans.produce(new AdditionalBeanBuildItem.Builder()
+                                        .addBeanClasses(diameterServices)
+                                        .setUnremovable()
+                                        .setDefaultScope(DotNames.SINGLETON)
+                                        .build());
+    }
 
-		return configurator;
-	}
+    private static <T> SyntheticBeanBuildItem.ExtendedBeanConfigurator createSyntheticBean(String clientName, Class<T> type, DotName exposedType, boolean isDefaultConfig)
+    {
+        LOG.info("Creating Synthetic Bean to {} for @DiameterClient(\"{}\")", type.getSimpleName(), clientName);
+        SyntheticBeanBuildItem.ExtendedBeanConfigurator configurator = SyntheticBeanBuildItem.configure(type).scope(ApplicationScoped.class).unremovable().setRuntimeInit().addType(exposedType);
+
+        if (isDefaultConfig) {
+            configurator.defaultBean();
+            configurator.addQualifier(Default.class)
+                        .addQualifier(DiameterConfig.class);
+        } else {
+            configurator.addQualifier()
+                        .annotation(DiameterConfig.class)
+                        .addValue("value", clientName).done();
+        }
+
+        return configurator;
+    }
 }
